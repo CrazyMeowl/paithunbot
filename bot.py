08f1e0fe6fa7b6be1bfba313141e7ebcb98c6c1a
CrazyMeowl <nooctran9904@gmai.com>
2021-06-05 23:40:51 +0700

    .

--- bot.py
+++ bot.py
@@ -4,51 +4,7 @@ from discord.voice_client import VoiceClient
 from discord.utils import get
 import os
 from random import choice
-import youtube_dl
-#import random
 
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 client = commands.Bot(command_prefix = 'p.')
@@ -120,7 +76,7 @@ async def test(ctx):
 
 
 
-status = ['Nghe nhạc', 'rảnh', 'hehe']
+status = ['test1', 'test2', 'test3']
 
 
 @client.command(name='play', help='This command plays music')
@@ -148,7 +104,7 @@ async def stop(ctx):
     voice_client = ctx.message.guild.voice_client
     await voice_client.disconnect()
 
-@tasks.loop(seconds=2)
+@tasks.loop(seconds=10)
 async def change_status():
     await client.change_presence(activity=discord.Game(choice(status)))
 
